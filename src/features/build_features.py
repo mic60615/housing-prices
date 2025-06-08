5c9dc9e70ffd4e763ea20ee517bfac92475a3a20
Jamie Chang <jamie.chang@toyota.com>
2025-06-08 09:15:12 -0700

    feat: explore linear regression - add raw housing data - add Jupyter notebook - add data loader, apply z-score scaling - Train linear regression model up to degree 6 - Evaluate MSEs and RMSEs

---
+++ src/features/build_features.py
@@ -0,0 +1,10 @@
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from src.data import data_loader
+
+def build_features(df):
+    data_loader.load_house_csv()
+    return
+
+
+##use for quick testing
